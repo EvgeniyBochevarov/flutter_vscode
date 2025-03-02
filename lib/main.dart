b226068b6be49dfd2a04b4770dde477a21849e7a
EvgeniyBochevarov <bochevarov@gmail.com>
2025-03-02 14:19:25 +0200

    НоваяВеткаИзменение

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times Евгений:'),
+            const Text(
+              'You have pushed the button this many times Евгений Новая ветка:',
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
